f7037a36ea8ebb610338d7b0bd87c3869eb575ac
DANILO-PC\Danilo Vucetic <danilo.vucetic@yahoo.ca>
2018-10-08 18:02:59 -0400

    Adding new files to test the UDP comms between the boards.
    Dan

---
+++ RPi_UDP_Comms/UDPReceiver.java
@@ -0,0 +1,40 @@
+//  Chance Abemba 101011372; Danilo Vucetic 100999548; Jacob Martin 101000849
+
+import java.net.*;
+
+public class UDPReceiver {
+
+	private final static int PACKETSIZE = 100 ;
+
+	public static void main( String args[] )
+	{ 
+	      // Check the arguments
+	      if( args.length != 1 )
+	      {
+	         System.out.println( "usage: UDPReceiver port" ) ;
+	         return ;
+	      }
+	      try
+	      {
+	         // Convert the argument to ensure that is it valid
+	         int port = Integer.parseInt( args[0] ) ;
+
+	         // Construct the socket
+	         DatagramSocket socket = new DatagramSocket( port ) ;
+
+	         for( ;; )
+	         {
+		        System.out.println( "Receiving on port " + port ) ;
+		        DatagramPacket packet = new DatagramPacket( new byte[PACKETSIZE], PACKETSIZE ) ;
+	            socket.receive( packet ) ;
+
+	            System.out.println( packet.getAddress() + " " + packet.getPort() + ": " + new String(packet.getData()).trim() ) ;
+	        }  
+	     }
+	     catch( Exception e )
+	     {
+	        System.out.println( e ) ;
+	     }
+  }
+}
+
