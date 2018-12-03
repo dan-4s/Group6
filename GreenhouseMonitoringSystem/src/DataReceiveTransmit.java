2afa129837917d1f508dd085520e2d57343f84f7
Jacob <jacob_martin@rogers.com>
2018-12-02 19:10:13 -0500

    error handleing changes

--- GreenhouseMonitoringSystem/src/DataReceiveTransmit.java
+++ GreenhouseMonitoringSystem/src/DataReceiveTransmit.java
@@ -61,13 +61,9 @@ public class DataReceiveTransmit implements Runnable{
 				        } catch (InterruptedException e) {
 					     e.printStackTrace();
 			          	}
-					System.out.println("CHECK 1");
 					tempJSON= serialPort.getSerialJSON();
-					System.out.println("CHECK 2");
 					struct.setRelativeHumidity((float)Float.parseFloat(tempJSON.getString("humidity")));
-					System.out.println("CHECK 3");
 					struct.setTemperature((float)Float.parseFloat(tempJSON.getString("temperature")));
-					System.out.println("CHECK 4");
 					//struct.setJSON(tempJSON);
 				}catch (Exception e) {
 				        e.printStackTrace();
--- GreenhouseMonitoringSystem/src/TwoWaySerialComm.java
+++ GreenhouseMonitoringSystem/src/TwoWaySerialComm.java
@@ -9,44 +9,36 @@ class TwoWaySerialComm implements Runnable{
     
     public void run()
     {
-    	 try {
-    		SerialPort comPort = SerialPort.getCommPorts()[0];
-    		comPort.openPort();
-       	 
-	         String last="";
-	         String current;
-	         String temp = null;
-
-	         while (true)
-	            {
-			System.out.println("TWOWAY HERE1");
-			while (comPort.bytesAvailable() == 0){
-			    System.out.println("TWOWAY HERE2");
-			    Thread.sleep(3000);
-			}
-			byte[] readBuffer = new byte[comPort.bytesAvailable()];
-			current = new String(readBuffer);
-			System.out.println("JSON = "+ current);
-			if(!current.equals(last)) {
-			    temp = new String(readBuffer);
-			    this.jsonObj = new JSONObject(temp);
-			    System.out.println("recived:   "+temp);
-			    last = current;
-			}
-			System.out.println("TWOWAY HERE3");
-		    }
-    	 } catch (ArrayIndexOutOfBoundsException exception) {
-            // Output expected ArrayIndexOutOfBoundsException.
-        	System.out.println("can not Read from serialPort: Most likely sensor not connected");
-        	exception.printStackTrace();
-        	//return;
-        } catch (Exception exception) {
-            // Output unexpected Exceptions.
-        	exception.printStackTrace();
-        	//return;
-        	
-        }
-    	
+    	  SerialPort comPort = SerialPort.getCommPorts()[0];
+          comPort.openPort();
+          String last="";
+          String current;
+          String temp = null;
+    
+       
+          try {
+              while (true)
+              {
+              	
+                  while (comPort.bytesAvailable() == 0)
+                      Thread.sleep(3000);
+                  byte[] readBuffer = new byte[comPort.bytesAvailable()];
+               
+                  int numRead = comPort.readBytes(readBuffer, readBuffer.length);
+                  current = new String(readBuffer);
+                  if(!current.equals(last))
+                	temp = new String(readBuffer);
+                	try{
+                		this.jsonObj = new JSONObject(temp);
+                		System.out.println("recived:   "+temp);
+                        last = current;
+                	}
+                	catch(Exception e){
+                		System.out.println(this.jsonObj);
+                	}  	
+              }
+          } catch (Exception e) { e.printStackTrace(); }
+          comPort.closePort();
     }
     
     protected synchronized JSONObject getSerialJSON() {
